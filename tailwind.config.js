2cc5a3e9101238c870eaa75c3273981decbebfb6
touchmarkdescience <mohan@touchmarkdes.com>
2022-08-19 12:41:47 +0530

    added nav-bar by referencing Hotstar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './index.html',
+    './css/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
